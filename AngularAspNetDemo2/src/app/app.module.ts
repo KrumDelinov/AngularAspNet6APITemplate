d030cd945333893c3d7aac0bc4cd39b8b1bca2c5
Krum Delinov <krum.delinov@gmail.com>
2022-03-18 15:38:09 +0200

    Remove test component

--- AngularAspNetDemo2/src/app/app.module.ts
+++ AngularAspNetDemo2/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AsdfComponent } from './asdf/asdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AsdfComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
